500b5aa0b05a6dd2dcd622ca3bebbef35f7c8489
m1-key <shubhamtiwari.tiwari84@gmail.com>
2021-09-19 15:41:50 +0530

    Added various packages like magit,lsp and many more.

--- init.el
+++ init.el
@@ -1,5 +1,15 @@
-(setq inhibit-startup-message t)
 
+;;;        ;;;      ;;;  ;;    ;;  ;;;;;;;  ;;      ;;
+;; ;;    ;; ;;    ;; ;;  ;;   ;;   ;;        ;;    ;;
+;;  ;;  ;;  ;;   ;;  ;;  ;;  ;;    ;;         ;;  ;;
+;;    ;;    ;;  ;;   ;;  ;; ;;     ;;;;;;;      ;;
+;;          ;;       ;;  ;;  ;;    ;;           ;;
+;;          ;;       ;;  ;;   ;;   ;;           ;;
+;;          ;;       ;;  ;;    ;;  ;;;;;;;      ;;
+
+; ------------------------------------------------------------------------
+
+(setq inhibit-startup-message t)
 (scroll-bar-mode -1)
 (tool-bar-mode -1)       ;Disable toolbar
 (tooltip-mode -1)        ;Disable tooltip
@@ -8,9 +18,6 @@
 ;; Set uo the visible bell
 (setq visible-bell t)
 
-;;Set theme
-(load-theme 'tango-dark)
-
 ;; Initialize package sources
 (require 'package)
 
@@ -18,6 +25,13 @@
                          ("melpa-stable" . "https://stable.melpa.org/packages/")
                          ("org" . "https://orgmode.org/elpa/")
                          ("elpa" . "https://elpa.gnu.org/packages/")))
+
+;; Set default font
+(set-face-attribute 'default nil
+                    :family "FantasqueSansMono Nerd Font Mono"
+                    :height 110
+                    :weight 'normal
+                    :width 'normal)
 
 (package-initialize)
 (unless package-archive-contents
@@ -32,7 +46,24 @@
 
 (use-package command-log-mode)
 
-(use-package doom-themes)
+(use-package doom-themes
+  :ensure t
+  :config
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'doom-gruvbox t)
+
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  ;; Enable custom neotree theme (all-the-icons must be installed!)
+  (doom-themes-neotree-config)
+  ;; or for treemacs users
+  (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
+  (doom-themes-treemacs-config)
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
+
 
 (column-number-mode)
 (global-display-line-numbers-mode t)
@@ -103,3 +134,65 @@
   ([remap describe-command] . helpful-command)
   ([remap describe-key] . helpful-key))
 
+(use-package magit
+  :bind ("C-M-;" . magit-status)
+  :commands (magit-status magit-get-current-branch)
+  :custom
+  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
+
+(use-package org
+  :config
+  (setq org-ellipsis " ▾"))
+
+;; Replace list hyphen with dot
+(font-lock-add-keywords 'org-mode
+                        '(("^ *\\([-]\\) "
+                           (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+
+(use-package org-bullets
+  :after org
+  :hook (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+
+(use-package fish-completion
+  :hook (eshell-mode . fish-completion-mode))
+
+(use-package term
+  :config
+  (setq explicit-shell-file-name "fish"))
+
+
+(use-package eterm-256color
+  :hook (term-mode . eterm-256color-mode))
+
+
+
+(defun m1key/lsp-mode-setup ()
+  (setq lsp-headerline-breadcrumb-segments '(path-up-to-project file symbols))
+  (lsp-headerline-breadcrumb-mode))
+
+(use-package lsp-mode
+  :commands (lsp lsp-deferred)
+  :hook (lsp-mode . m1key/lsp-mode-setup)
+  :init
+  (setq lsp-keymap-prefix "C-c l")  ;; Or 'C-l', 's-l'
+  :config
+  (lsp-enable-which-key-integration t))
+
+
+(use-package python-mode
+  :ensure t
+  :hook (python-mode . lsp-deferred)
+  :custom
+  (dap-python-debugger 'debugpy)
+  :config
+  (require 'dap-python))
+
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode))
+
+;; My Keys
+(global-set-key (kbd "M-z") 'vterm)
+(global-set-key (kbd "C-c f") 'format-all-buffer)
