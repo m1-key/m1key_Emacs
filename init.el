872671db9605ba9aeb656118b7d9ca473a181a63
m1-key <shubhamtiwari.tiwari84@gmail.com>
2021-10-09 19:21:51 +0530

    Readme update

--- init.el
+++ init.el
@@ -28,6 +28,13 @@
                          ("org" . "https://orgmode.org/elpa/")
                          ("elpa" . "https://elpa.gnu.org/packages/")))
 
+;; Set default font
+(set-face-attribute 'default nil
+                    :family "FantasqueSansMono Nerd Font Mono"
+                    :height 110
+                    :weight 'normal
+                    :width 'normal)
+
 (package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
@@ -40,13 +47,6 @@
 (setq use-package-always-ensure t)
 
 (use-package command-log-mode)
-
-;; Set default font
-(set-face-attribute 'default nil
-                    :family "FantasqueSansMono Nerd Font Mono"
-                    :height 110
-                    :weight 'normal
-                    :width 'normal)
 
 (use-package doom-themes
   :ensure t
@@ -66,16 +66,15 @@
   ;; Corrects (and improves) org-mode's native fontification.
   (doom-themes-org-config))
 
-(use-package doom-modeline
-  :ensure
-  :init(doom-modeline-mode 1))
 
-;; Source code blocks shortcuts
+;; This is shortcut to add source codes
 (require 'org-tempo)
 
 (add-to-list 'org-structure-template-alist '("sh" . "src sh"))
 (add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
+(add-to-list 'org-structure-template-alist '("li" . "src lisp"))
 (add-to-list 'org-structure-template-alist '("py" . "src python"))
+(add-to-list 'org-structure-template-alist '("json" . "src json"))
 
 (column-number-mode)
 (global-display-line-numbers-mode t)
@@ -88,15 +87,6 @@
 		shell-mode-hook))
   (add-hook mode(lambda()(display-line-numbers-mode 0))))
 
-(use-package counsel
-  :bind (("M-x" . counsel-M-x)
-         ("C-x b" . counsel-ibuffer)
-         ("C-x C-f" . counsel-find-file)
-         ("C-M-l" . counsel-imenu)
-         :map minibuffer-local-map
-         ("C-r" . 'counsel-minibuffer-history))
-  :config
-  (setq ivy-initial-inputs-alist nil)) ;; Don't start searches with ^
 
 (use-package ivy
   :diminish
@@ -117,6 +107,10 @@
   :config
   (ivy-mode 1))
 
+(use-package doom-modeline
+  :ensure
+  :init(doom-modeline-mode 1))
+
 (use-package rainbow-delimiters
   :hook (prog-mode . rainbow-delimiters-mode))
 
@@ -124,11 +118,21 @@
   :init (which-key-mode)
   :diminish which-key-mode
   :config
-  (setq which-key-idle-delay 0.3))
+  (setq which-key-idle-delay 0.5))
 
 (use-package ivy-rich
   :init
   (ivy-rich-mode 1))
+
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+         ("C-x b" . counsel-ibuffer)
+         ("C-x C-f" . counsel-find-file)
+         ("C-M-l" . counsel-imenu)
+         :map minibuffer-local-map
+         ("C-r" . 'counsel-minibuffer-history))
+  :config
+  (setq ivy-initial-inputs-alist nil)) 
 
 (use-package helpful
   :custom
@@ -141,13 +145,15 @@
   ([remap describe-command] . helpful-command)
   ([remap describe-key] . helpful-key))
 
+
 (use-package magit
   :bind ("C-M-;" . magit-status)
   :commands (magit-status magit-get-current-branch)
   :custom
   (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
 
-(use-package forge)
+(use-package forge
+  :after magit)
 
 (use-package org
   :config
@@ -174,8 +180,6 @@
 
 (use-package eterm-256color
   :hook (term-mode . eterm-256color-mode))
-
-
 
 (defun m1key/lsp-mode-setup ()
   (setq lsp-headerline-breadcrumb-segments '(path-up-to-project file symbols))
